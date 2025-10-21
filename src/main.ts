2a2c424b3e52cc469e395c7ee9c0fb46f904714a
cristian <cristiandavidgarcia10@gmail.com>
2025-10-21 16:33:56 -0500

    driver y slots

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // 👇 prefijo global uniforme: todas las rutas quedan bajo /api/...
+
   app.setGlobalPrefix('api');
 
   await app.listen(3000);
